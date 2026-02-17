dd3e76372b6e0eb92f7d3f96ad8ce4179a947a39
Javier Pitalua <javier.pitalua@live.com>
2026-02-17 11:43:40 -0600

    se agrega la base address de la api

--- client/api/core/OpenAPI.ts
+++ client/api/core/OpenAPI.ts
@@ -20,7 +20,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-    BASE: '',
+    BASE: 'https://dev-api.tikko.mx',
     VERSION: '1',
     WITH_CREDENTIALS: false,
     CREDENTIALS: 'include',
